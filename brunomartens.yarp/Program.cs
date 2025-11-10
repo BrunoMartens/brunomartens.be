da1d32b51b31709999c350473c7c225bfc88c0ef
Bruno Martens <brunomartens@telenet.be>
2025-11-10 16:56:59 +0100

    yarp + compose dev

--- brunomartens.yarp/Program.cs
+++ brunomartens.yarp/Program.cs
@@ -1,6 +1,27 @@
+using Yarp.ReverseProxy.Transforms;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    /*.AddTransforms(transformBuilderContext =>
+    {
+        transformBuilderContext.AddRequestTransform(async transformContext => {
+
+            if (string.Equals("authPolicy", transformBuilderContext.Route.AuthorizationPolicy))
+            {
+                var tokenService = new TokenService();
+                var token = await tokenService.GetToken(oidcConfig["ClientId"], oidcConfig["ClientSecret"], "api://brunomartens.be/.default", oidcConfig["TenantId"]);
+
+                var headers = transformContext.ProxyRequest.Headers;
+                headers.Add("Authorization", $"Bearer {token}");
+            }
+        });
+    });*/
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapReverseProxy();
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
